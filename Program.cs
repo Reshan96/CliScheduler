f12a1f096e2632943e6bcc6555b98483061eb4ff
CS\Reshan.D <reshan.d@creativesoftware.com>
2024-07-02 14:24:50 +0530

    Batchfile updated for scheduled run

--- Program.cs
+++ Program.cs
@@ -1,71 +1,35 @@
-﻿int[] times = { 800, 1200, 1600, 2000 };
-int diff = 0;
-MedicineSchduler();
-Console.ReadKey();
-void MedicineSchduler()
+﻿using System.CommandLine;
+
+class Program
 {
-    Console.WriteLine("Enter current GMT");
-    int currentGMT = Convert.ToInt32(Console.ReadLine());
-
-    Console.WriteLine("Current Medicine Schedule:");
-    DisplayMedicineTime(times);
-
-    Console.WriteLine();
-
-    Console.WriteLine("Enter new GMT");
-    int newGMT = Convert.ToInt32(Console.ReadLine());
-
-    if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Invalid GMT");
-    }
-    else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
-    {
-        diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
-        AdjustTimes();
-    }
-    else
-    {
-        diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
-        AdjustTimes();
-    }
-
-    Console.WriteLine("New Medicine Schedule:");
-    DisplayMedicineTime(times);
-
-    Console.WriteLine();
-
-}
-
-void DisplayMedicineTime(int[] timeInts)
-{
-    /* Format and display medicine times */
-    foreach (int val in timeInts)
-    {
-        string time = val.ToString();
-        int len = time.Length;
-
-        if (len >= 3)
+        var rootCommand = new RootCommand("Sample command-line app");
+        rootCommand.SetHandler(() =>
         {
-            time = time.Insert(len - 2, ":");
-        }
-        else if (len == 2)
-        {
-            time = time.Insert(0, "0:");
-        }
-        else
-        {
-            time = time.Insert(0, "0:0");
-        }
+            Console.WriteLine("Hello world!");
+            Console.ReadKey();
+        });
+        
+        var subCommand1 = new Command("view", "View items");
+        rootCommand.Add(subCommand1);
 
-        Console.Write($"{time} ");
-    }
-}
+        var delayOption = new Option<int>
+        (
+            name: "--delay",
+            description: "option argument passed as int",
+            getDefaultValue: () => 42
+        );
 
-void AdjustTimes()
-{
-    for (int i = 0; i < times.Length; i++)
-    {
-        times[i] = ((times[i] + diff)) % 2400;
+        subCommand1.Add(delayOption);
+        subCommand1.SetHandler((delayOptionValue) =>
+        {
+            Console.WriteLine($"--delay = {delayOptionValue}");
+        },delayOption);
+
+        await rootCommand.InvokeAsync(args);
+
+
     }
+
 }
